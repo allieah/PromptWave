805f678dd3cde400c7259207eec378348907ea11
allieah <allieah10@gmail.com>
2024-09-12 15:38:48 +0530

    implemented edit and delete functionality

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
@@ -6,34 +7,51 @@ import { useRouter } from "next/navigation";
 import Profile from "@components/Profile";
 
 const MyProfile = () => {
+  const router = useRouter();
   const { data: session } = useSession();
-  const [posts, setPosts] = useState([]);
+
+  const [myPosts, setMyPosts] = useState([]);
 
   useEffect(() => {
     const fetchPosts = async () => {
       const response = await fetch(`/api/users/${session?.user.id}/posts`);
       const data = await response.json();
 
-      setPosts(data);
+      setMyPosts(data);
     };
-    // console.log(posts);
+
     if (session?.user.id) fetchPosts();
-  }, []);
+  }, [session?.user.id]);
 
-  const handleEdit = () => {
-    // router.push(`/update-prompt?id=${post._id}`);
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`);
   };
 
-  const handleDelete = async () => {
-    // const hasConfirmed = confirm(
-    //   "Are you sure you want to delete this prompt?"
-    // );
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm(
+      "Are you sure you want to delete this prompt?"
+    );
+
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: "DELETE",
+        });
+
+        const filteredPosts = myPosts.filter((item) => item._id !== post._id);
+
+        setMyPosts(filteredPosts);
+      } catch (error) {
+        console.log("ereee", error);
+      }
+    }
   };
+
   return (
     <Profile
       name="My"
       desc="Welcome to your personalized profile page. Share your exceptional prompts and inspire others with the power of your imagination"
-      data={posts}
+      data={myPosts}
       handleEdit={handleEdit}
       handleDelete={handleDelete}
     />
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -5,36 +5,52 @@ import Image from "next/image";
 import { useSession } from "next-auth/react";
 import { usePathname, useRouter } from "next/navigation";
 
-const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
-  const creatorImage = post?.creator?.image || "/default-image.png"; // Fallback to a default image if undefined
-  const creatorUsername = post?.creator?.username || "Unknown User"; // Fallback to a default name if undefined
-  const creatorEmail = post?.creator?.email || "No Email Provided"; // Fallback to a default email if undefined
+const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
+  const { data: session } = useSession();
+  const pathName = usePathname();
+  const router = useRouter();
 
-  const [copied, setcopied] = useState("");
-  const handleCopy = () => {
-    setcopied(post.prompt);
-    navigator.clipboard.writeText(post.prompt);
-    setTimeout(() => setcopied(""), 3000);
+  const [copied, setCopied] = useState("");
+
+  const handleProfileClick = () => {
+    console.log(post);
+
+    if (post.creator._id === session?.user.id) return router.push("/profile");
+
+    router.push(`/profile/${post.creator._id}?name=${post.creator.username}`);
   };
-  console.log("Post Data:", post); // Log the post data
+
+  const handleCopy = () => {
+    setCopied(post.prompt);
+    navigator.clipboard.writeText(post.prompt);
+    setTimeout(() => setCopied(false), 3000);
+  };
+
   return (
     <div className="prompt_card">
       <div className="flex justify-between items-start gap-5">
-        <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
+        <div
+          className="flex-1 flex justify-start items-center gap-3 cursor-pointer"
+          onClick={handleProfileClick}
+        >
           <Image
-            src={creatorImage}
+            src={post.creator.image}
             alt="user_image"
             width={40}
             height={40}
             className="rounded-full object-contain"
           />
+
           <div className="flex flex-col">
-            <h3 className="font-satoshi font-semibold">{post.username}</h3>
+            <h3 className="font-satoshi font-semibold text-gray-900">
+              {post.creator.username}
+            </h3>
             <p className="font-inter text-sm text-gray-500">
-              {/* {post.creator.email} */}
+              {post.creator.email}
             </p>
           </div>
         </div>
+
         <div className="copy_btn" onClick={handleCopy}>
           <Image
             src={
@@ -48,13 +64,31 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
           />
         </div>
       </div>
+
       <p className="my-4 font-satoshi text-sm text-gray-700">{post.prompt}</p>
       <p
         className="font-inter text-sm blue_gradient cursor-pointer"
         onClick={() => handleTagClick && handleTagClick(post.tag)}
       >
-        {post.tag}
+        #{post.tag}
       </p>
+
+      {session?.user.id === post.creator._id && pathName === "/profile" && (
+        <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+          <p
+            className="font-inter text-sm green_gradient cursor-pointer"
+            onClick={handleEdit}
+          >
+            Edit
+          </p>
+          <p
+            className="font-inter text-sm orange_gradient cursor-pointer"
+            onClick={handleDelete}
+          >
+            Delete
+          </p>
+        </div>
+      )}
     </div>
   );
 };
