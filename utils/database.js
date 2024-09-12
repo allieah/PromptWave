ae8f98dd3f07224490cda3718d1e922133977c46
allieah <allieah10@gmail.com>
2024-09-12 17:48:17 +0530

    database

--- utils/database.js
+++ utils/database.js
@@ -26,31 +26,27 @@
 // };
 import mongoose from "mongoose";
 
-let isConnected = false; // Track the connection state
+let isConnected = false; // track the connection
 
 export const connectToDB = async () => {
-  // Set strict query mode for Mongoose
   mongoose.set("strictQuery", true);
 
-  // Return early if already connected
   if (isConnected) {
     console.log("MongoDB is already connected");
     return;
   }
 
   try {
-    // Connect to MongoDB
-
     await mongoose.connect(process.env.MONGODB_URI, {
-      dbName: "share_prompt", // Match this with the database name if needed
+      dbName: "share_prompt",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
 
-    // Mark as connected
     isConnected = true;
+
     console.log("MongoDB connected");
   } catch (error) {
-    // Log and throw the error
-    console.error("Error connecting to MongoDB:", error);
-    throw new Error("Failed to connect to MongoDB");
+    console.log(error);
   }
 };
